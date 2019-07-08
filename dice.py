295c9c6406d85650cb4ea407ba97259df7a7120f
Kai Yamada <gpioblink@aol.com>
2019-07-08 23:22:41 +0900

    first commit

---
+++ dice.py
@@ -0,0 +1,8 @@
+import random
+
+print("Rolling the dice...")
+die1 = random.randint(1,6)
+die2 = random.randint(1,6)
+print("Die 1:", die1)
+print("Die 2:", die2)
+print("Total value:", die1 + die2)
